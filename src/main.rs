d2a7ad2eb26f6d3d9354f2709029b5e963f61081
Xvolks <xvolks@gmail.com>
2026-09-21 16:17:17 +0200

    Pack same instructions into one: ++++ => Inc(4)

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ use std::env::args;
 
 #[derive(Clone, Debug, PartialEq)]
 enum Op {
-    INC(u32),
-    DEC(u32),
-    LEFT(u32),
-    RIGHT(u32),
-    OUT(u32),
-    IN(u32),
-    JZ(usize),
-    JNZ(usize),
-    Nop,
+    Inc(u32),
+    Dec(u32),
+    Left(u32),
+    Right(u32),
+    Out(u32),
+    In(u32),
+    Jz(usize),
+    Jnz(usize),
 }
 
 impl Op {
@@ -18,35 +17,50 @@ impl Op {
         let mut ops: Vec<Op> = vec![];
         let mut stack = Stack::new();
 
-        let mut comments = 0;
-        for (i, c) in source.chars().enumerate() {
+        let mut _comments = 0;
+        let mut operand = 0;
+        let mut last_op = None;
+        let mut i = 0;
+        for c in source.chars() {
             let op = match c {
-                '+' => Op::INC(1),
-                '-' => Op::DEC(1),
-                '<' => Op::LEFT(1),
-                '>' => Op::RIGHT(1),
-                '.' => Op::OUT(1),
-                ',' => Op::IN(1),
+                '+' => Some(Op::Inc(0)),
+                '-' => Some(Op::Dec(0)),
+                '<' => Some(Op::Left(0)),
+                '>' => Some(Op::Right(0)),
+                '.' => Some(Op::Out(0)),
+                ',' => Some(Op::In(0)),
                 '[' => {
-                    stack.push(i - comments);
-                    Op::JZ(stack.len())
+                    stack.push(i + 1);
+                    Some(Op::Jz(stack.len()))
                 }
                 ']' => {
                     let el = stack.pop();
                     // Need to backpatch the [ instruction, so the destination addr is myself.
-                    let _ = std::mem::replace(&mut ops[el], Op::JZ(i - comments));
-                    Op::JNZ(el)
+                    let _ = std::mem::replace(&mut ops[el], Op::Jz(i));
+                    Some(Op::Jnz(el))
                 }
                 _ => {
                     /* This is a comment */
-                    comments += 1;
-                    Op::Nop
+                    _comments += 1;
+                    None
                 }
             };
-            if op != Op::Nop {
-                ops.push(op);
+            if let Some(op) = op {
+                if last_op.is_none()
+                    || std::mem::discriminant(&last_op.clone().unwrap())
+                        == std::mem::discriminant(&op)
+                {
+                    last_op = Some(op);
+                } else {
+                    ops.push(create_instruction(operand, &last_op));
+                    operand = 0;
+                    i += 1;
+                    last_op = Some(op);
+                }
+                operand += 1;
             }
         }
+        ops.push(create_instruction(operand, &last_op));
         ops
     }
 
@@ -56,11 +70,23 @@ impl Op {
         for (i, op) in ops.iter().enumerate() {
             println!("{i:04}{}- {:?}", " ".repeat(level), op);
             match op {
-                Op::JZ(_) => level += 1,
-                Op::JNZ(_) => level -= 1,
+                Op::Jz(_) => level += 1,
+                Op::Jnz(_) => level -= 1,
                 _ => {}
             }
         }
+    }
+}
+
+fn create_instruction(operand: u32, last_op: &Option<Op>) -> Op {
+    match last_op.clone().unwrap() {
+        Op::Inc(_) => Op::Inc(operand),
+        Op::Dec(_) => Op::Dec(operand),
+        Op::Left(_) => Op::Left(operand),
+        Op::Right(_) => Op::Right(operand),
+        Op::In(_) => Op::In(operand),
+        Op::Out(_) => Op::Out(operand),
+        _ => last_op.clone().unwrap(),
     }
 }
 
@@ -84,16 +110,16 @@ impl Stack {
 }
 
 type Mem = Vec<u8>;
-const MAX_MEM:usize = 1024;
+const MAX_MEM: usize = 1024;
 
-struct CPU {
+struct Cpu {
     memory: Mem,
     ops: Vec<Op>,
     ip: usize,
     head: usize,
 }
 
-impl CPU {
+impl Cpu {
     pub fn new(ops: Vec<Op>) -> Self {
         Self {
             memory: vec![0; MAX_MEM],
@@ -108,22 +134,24 @@ impl CPU {
         loop {
             // println!("Executing: {:?}", self.ops[self.ip]);
             match self.ops[self.ip] {
-                Op::DEC(n) => self.memory[self.head] = self.memory[self.head].wrapping_sub(n as u8),
-                Op::INC(n) => self.memory[self.head] = self.memory[self.head].wrapping_add(n as u8),
-		// This might break if the program wants to access the memory before addr 0
-                Op::LEFT(n) => self.head -= n as usize,
-		// This might break if the program wants to access the memory after addr MAX_MEM
-                Op::RIGHT(n) => self.head += n as usize,
-                Op::OUT(n) => {
+                Op::Dec(n) => self.memory[self.head] = self.memory[self.head].wrapping_sub(n as u8),
+                Op::Inc(n) => self.memory[self.head] = self.memory[self.head].wrapping_add(n as u8),
+                // This might break if the program wants to access the memory before addr 0
+                Op::Left(n) => self.head -= n as usize,
+                // This might break if the program wants to access the memory after addr MAX_MEM
+                Op::Right(n) => self.head += n as usize,
+                Op::Out(n) => {
                     for _ in 0..n {
                         print!("{}", self.memory[self.head] as char);
                     }
                 }
-                Op::IN(_n) => {
-                    let s = getch.getch().unwrap();
-                    self.memory[self.head] = s;
+                Op::In(n) => {
+                    for _ in 0..n {
+                        let s = getch.getch().unwrap();
+                        self.memory[self.head] = s;
+                    }
                 }
-                Op::JZ(addr) => {
+                Op::Jz(addr) => {
                     if self.memory[self.head] == 0 {
                         // println!("Jump to {addr}, Memory: {:?}", self.memory);
                         self.ip = addr + 1;
@@ -132,7 +160,7 @@ impl CPU {
                         // println!("Nop, Memory: {:?}", self.memory)
                     }
                 }
-                Op::JNZ(addr) => {
+                Op::Jnz(addr) => {
                     if self.memory[self.head] != 0 {
                         // println!("Jump to {addr}, Memory: {:?}", self.memory);
                         self.ip = addr + 1;
@@ -140,9 +168,6 @@ impl CPU {
                     } else {
                         // println!("Nop, Memory: {:?}", self.memory)
                     }
-                }
-                _ => {
-                    panic!("Op unknown")
                 }
             };
             self.ip += 1;
@@ -234,7 +259,7 @@ pub fn exec_source(source: &str) {
     }
     #[cfg(debug_assertions)]
     Op::dump(&ops);
-    CPU::new(ops).execute();
+    Cpu::new(ops).execute();
 }
 
 fn main() {
